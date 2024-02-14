1239b99b4488076f0e97430dadf320989d31b979
scottmacdonnell <scottmacdonnell@protonmail.com>
2024-02-14 13:11:49 -0500

    Implement Wiiboard class

--- server/rewritten.py
+++ server/rewritten.py
@@ -4,26 +4,64 @@ import bluetooth
 import sys
 import subprocess
 
-def main():
-  address = input("Enter the Wiiboard address (or press Enter to discover): ")
+class Wiiboard:
+  def __init__(self, processor):
+    self.address = None
 
-  if not address:
-    print("Discovering board...")
-    
-    ## DISCOVER FUNCTION - Start
+    try:
+      self.receivesocket = bluetooth.BluetoothSocket(bluetooth.L2CAP)
+      self.controlsocket = bluetooth.BluetoothSocket(bluetooth.L2CAP)
+    except ValueError:
+      raise Exception("Error: Bluetooth not found")
 
+
+  def discover(self):
     print("Press the red sync button on the board now")
+
+    address = None
+
     bluetooth_devices = bluetooth.discover_devices(duration = 6, lookup_names = True)
 
     for bluetooth_device in bluetooth_devices:
       if bluetooth_device[1] == "Nintendo RVL-WBC-01":
         address = bluetooth_device[0]
         print("Found Wiiboard at address " + address)
-      if address is None:
-        print("No Wiiboards discovered")
 
-    ## DISCOVER FUNCTION - End
+    if address is None:
+      print("No Wiiboards discovered")
+    
+    return address
+  
 
+    
+
+def main():
+  board = Wiiboard()
+
+  address = input("Enter the Wiiboard address (or press Enter to discover): ")
+
+  if not address:
+    print("Discovering board...")
+    address = board.discover()
+    
+    try:
+      # Disconnect already-connected devices.
+      # This is basically Linux black magic just to get the thing to work.
+      subprocess.check_output(["bluez-test-input", "disconnect", address], stderr=subprocess.STDOUT)
+      subprocess.check_output(["bluez-test-input", "disconnect", address], stderr=subprocess.STDOUT)
+    except:
+      pass
+
+    print("Trying to connect...")
+
+    ## CONNECT FUNCTION - Start
+
+    # if address is None:
+    #   print("Non existent address")
+    #   return
+
+
+  
 
 
 
