1c5a916e4477da52f06ecddd6a9c45f1d97416fa
scottmacdonnell <scottmacdonnell@protonmail.com>
2024-02-14 12:50:47 -0500

    Add rewritten.py

---
+++ server/rewritten.py
@@ -0,0 +1,27 @@
+import bluetooth
+
+def main():
+  address = input("Enter the Wiiboard address (or press Enter to discover): ")
+
+  if not address:
+    print("Discovering board...")
+    
+    ## DISCOVER FUNCTION - Start
+
+    print("Press the red sync button on the board now")
+    bluetooth_devices = bluetooth.discover_devices(duration = 6, lookup_names = True)
+
+    for bluetooth_device in bluetooth_devices:
+      if bluetooth_device[1] == "Nintendo RVL-WBC-01":
+        address = bluetooth_device[0]
+        print("Found Wiiboard at address " + address)
+      else:
+        print("No Wiiboards discovered")
+
+    ## DISCOVER FUNCTION - End
+
+    
+
+
+if __name__ == "__main__":
+  main()
