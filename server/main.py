165e53fc141e58f451e97824d6e239c6208c9cd9
scottmacdonnell <scottmacdonnell@protonmail.com>
2024-02-21 12:36:02 -0500

    Scott's next shot in the dark

---
+++ server/main.py
@@ -0,0 +1,10 @@
+import RPi.GPIO as GPIO
+from hx711 import HX711
+
+GPIO.setmode(GPIO.BCM)
+
+hx = HX711(dout_pin=6, pd_sck_pin=5)
+
+while True:
+  reading = hx.get_raw_data_mean()
+  print(reading)
