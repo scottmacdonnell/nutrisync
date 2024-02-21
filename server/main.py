ca798f1cb9d125a4b53b2b8b92e19a99b08c6646
scottmacdonnell <scottmacdonnell@protonmail.com>
2024-02-21 13:07:13 -0500

    Major changes

--- server/main.py
+++ server/main.py
@@ -1,10 +1,14 @@
 import RPi.GPIO as GPIO
-from hx711 import HX711
 
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 
-hx = HX711(dout_pin=29, pd_sck_pin=31)
+GPIO.setwarnings(False)
 
-while True:
-  reading = hx.get_raw_data_mean()
-  print(reading)
+GPIO.setup(6, GPIO.OUT)
+GPIO.setup(5, GPIO.IN)
+
+# hx = HX711(dout_pin=5, pd_sck_pin=6)
+
+GPIO.input(5)
+
+GPIO.cleanup()
