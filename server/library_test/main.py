1ec9af126666b7ca39b633f99df83b408af80f00
Scott MacDonnell <scottsmacdonnell@gmail.com>
2024-02-26 17:47:42 -0500

    Minor changes

--- server/library_test/main.py
+++ server/library_test/main.py
@@ -4,7 +4,7 @@ from hx711 import HX711
 # Set GPIO mode
 GPIO.setmode(GPIO.BCM)
 
-hx = HX711(dout_pin=6, pd_sck_pin=5)
+hx = HX711(dout_pin=5, pd_sck_pin=6)
 
 reading = hx.get_raw_data_mean()
 print(reading)
