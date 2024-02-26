cb5db49e0338b2011ac8234ee996ca19a33bd7ed
Scott MacDonnell <scottsmacdonnell@gmail.com>
2024-02-26 16:17:39 -0500

    Minor changes

--- server/main.py
+++ server/main.py
@@ -1,54 +1,57 @@
 import RPi.GPIO as GPIO
 import time
 
-# Define GPIO pins
+# Define the pins we're using
 DOUT = 5
 PD_SCK = 6
 
-# Set up GPIO pins
+# We're using the BCM pin numbering
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(PD_SCK, GPIO.OUT)
-GPIO.setup(DOUT, GPIO.IN)
 
+# Define DOUT as an input pin and PD_SCK as an output pin
+GPIO.setup(DOUT, GPIO.IN)
+GPIO.setup(PD_SCK, GPIO.OUT)
+
+# This function will read data from the HX711
 def read_HX711():
     count = 0
+    # Make sure the chip is ready
     GPIO.output(PD_SCK, False)
-    
-    # Wait for the HX711 to become ready
     while GPIO.input(DOUT) == 1:
-        pass
-    
-    # Read out 24-bit data from HX711
+        time.sleep(0.01)
+
+    # Read 24 bits of data
     for i in range(24):
         GPIO.output(PD_SCK, True)
         count = count << 1
+
         GPIO.output(PD_SCK, False)
-        if GPIO.input(DOUT) == 0:
+        if GPIO.input(DOUT) == 1:
             count += 1
-            
-    # Set the channel and gain factor for next reading
+
+    # This will trigger the next conversion
     GPIO.output(PD_SCK, True)
-    count ^= 0x800000  # XOR to flip the 25th bit
+    count = count ^ 0x800000
     GPIO.output(PD_SCK, False)
     
     return count
 
+# This function will convert the data to weight
 def get_weight():
-    # Read the data
     value = read_HX711()
-    
-    # TODO: Calibrate the following values as per your load cell and HX711
-    reference_unit = 1  # Set to the value obtained during calibration
-    offset = 0  # Set to the value obtained during calibration
-    
-    weight = (value - offset) / reference_unit
+    # Set your calibration factor here, which you'll need to determine experimentally
+    calibration_factor = -7050
+    weight = value / calibration_factor
     return weight
 
 try:
+    # Infinite loop to continuously check the weight
     while True:
-        total_weight = get_weight()
-        print(f"Total Weight: {total_weight} grams")
+        weight = get_weight()
+        print(f"Total Weight: {weight} grams")
         time.sleep(5)
+
+# Clean up cleanly on Ctrl+C
 except (KeyboardInterrupt, SystemExit):
     print("Cleaning up...")
     GPIO.cleanup()
