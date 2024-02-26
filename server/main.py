a3fd1d50b49f917de3ddd8848821493bebdb3347
Scott MacDonnell <scottsmacdonnell@gmail.com>
2024-02-26 15:59:42 -0500

    Update script and clean

--- server/main.py
+++ server/main.py
@@ -1,14 +1,60 @@
 import RPi.GPIO as GPIO
+import time
 
-GPIO.setmode(GPIO.BCM)
+DOUT = 5
+PD_SCK = 6
 
-GPIO.setwarnings(False)
+def read_HX711():
+    count = 0
+    GPIO.output(PD_SCK, False)
+    
+    # Wait for the HX711 to become ready
+    while GPIO.input(DOUT) == 1:
+        pass
+    
+    # Read out 24-bit data from HX711
+    for i in range(24):
+        GPIO.output(PD_SCK, True)
+        count = count << 1
+        GPIO.output(PD_SCK, False)
+        if GPIO.input(DOUT) == 0:
+            count += 1
+            
+    # Set the channel and gain factor for next reading
+    GPIO.output(PD_SCK, True)
+    count ^= 0x800000  # XOR to flip the 25th bit
+    GPIO.output(PD_SCK, False)
+    
+    return count
 
-GPIO.setup(6, GPIO.OUT)
-GPIO.setup(5, GPIO.IN)
 
-# hx = HX711(dout_pin=5, pd_sck_pin=6)
+def get_weight():
+    # Read the data
+    value = read_HX711()
+    
+    # TODO: Calibrate the following values as per your load cell and HX711
+    reference_unit = 1  # Set to the value obtained during calibration
+    offset = 0  # Set to the value obtained during calibration
+    
+    weight = (value - offset) / reference_unit
+    return weight
 
-GPIO.input(5)
 
-GPIO.cleanup()
+def main():
+    # Set up GPIO pins
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(PD_SCK, GPIO.OUT)
+    GPIO.setup(DOUT, GPIO.IN)
+    
+    try:
+        while True:
+            total_weight = get_weight()
+            print(f"Total Weight: {total_weight} grams")
+            time.sleep(5)
+    except (KeyboardInterrupt, SystemExit):
+        print("Cleaning up...")
+        GPIO.cleanup()
+
+
+if __name__ == "__main__":
+    main()
