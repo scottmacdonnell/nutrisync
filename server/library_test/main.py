0d9c6821bc397d7aeda876dbb58edb11a82d93af
Scott MacDonnell <scottsmacdonnell@gmail.com>
2024-02-26 17:32:10 -0500

    Testing

--- server/library_test/main.py
+++ server/library_test/main.py
@@ -1,10 +1,36 @@
-import RPi.GPIO as GPIO
+import time
 from hx711 import HX711
 
-GPIO.setmode(GPIO.BCM)
+def clean_and_exit():
+    print("Cleaning...")
+    if hx is not None:
+        hx.power_down()
+    print("Bye!")
+    sys.exit()
 
 hx = HX711(dout_pin=5, pd_sck_pin=6)
 
+# I'm setting the reference unit to 1 for now. You will need to calibrate your scale.
+# Each scale will need individual calibration.
+hx.set_reference_unit(1)
+
+hx.reset()
+hx.tare()
+
+print("Tare done! Add weight now...")
+
+# To get weight from the HX711 module
 while True:
-    reading = hx.get_raw_data_mean()
-    print(reading)
+    try:
+        val = hx.get_weight(5)
+        print(f"Weight: {val} grams")
+
+        # To get more accurate readings,
+        # you may want to discard the first few readings
+        # and average several readings together
+
+        hx.power_down()
+        hx.power_up()
+        time.sleep(0.5)
+    except (KeyboardInterrupt, SystemExit):
+        clean_and_exit()
