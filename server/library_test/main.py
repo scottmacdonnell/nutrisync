487fd04524f483a5a21d17cf57e24a848de7570d
Scott MacDonnell <scottsmacdonnell@gmail.com>
2024-02-26 17:46:13 -0500

    Minor changes

--- server/library_test/main.py
+++ server/library_test/main.py
@@ -1,26 +1,10 @@
 import RPi.GPIO as GPIO
-import time
 from hx711 import HX711
-
-def clean_and_exit():
-    print("Cleaning...")
-    GPIO.cleanup()  # Clean up GPIO to reset pins
-    print("Bye!")
-    sys.exit()
 
 # Set GPIO mode
 GPIO.setmode(GPIO.BCM)
 
 hx = HX711(dout_pin=5, pd_sck_pin=6)
 
-hx.zero()
-
-while True:
-    try:
-        val = hx.get_data_mean(readings=100)
-        print(f"Weight: {val} grams")
-        hx.power_down()
-        hx.power_up()
-        time.sleep(0.5)
-    except (KeyboardInterrupt, SystemExit):
-        clean_and_exit()
+reading = hx.get_raw_data_mean()
+print(reading)
