4b64e9f5f3107eeaab6bf124bc5a2cb9a51cd3bc
Scott MacDonnell <scottsmacdonnell@gmail.com>
2024-03-11 14:10:19 -0400

    Test status function

--- server/main.py
+++ server/main.py
@@ -1,8 +1,14 @@
 import RPi.GPIO as GPIO
 from hx711 import HX711
 
-def status(escape, message):
-    return '[' + escape + message + '\x1b[0m] '
+def status(format_code, message):
+    escape_code = '\033[0m'
+    message = message[:6]
+    total_spaces = 6 - len(message)
+    left_spaces = total_spaces // 2
+    right_spaces = total_spaces - left_spaces
+    s = ' ' * left_spaces + message + ' ' * right_spaces
+    return '[' + format_code + s + escape_code + '] '
     
 
 def main():
@@ -11,7 +17,7 @@ def main():
     
     hx.zero()
     
-    input('[\033[32m  OK  \033[0m] ' + 'Place known weight on scale and press enter')
+    input(status('\033[32m', 'OK') + 'Place known weight on scale and press enter')
     reading = hx.get_data_mean(readings=100)
     
     known_weight = float(input('Enter the known weight in grams and press enter: '))
