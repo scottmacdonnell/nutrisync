7ff2746ca74f893f5b9ffc0873801233d6601024
Scott MacDonnell <scottsmacdonnell@gmail.com>
2024-03-11 13:06:39 -0400

    Cleaning script

--- server/main.py
+++ server/main.py
@@ -1,50 +1,8 @@
 import RPi.GPIO as GPIO
-import time
 from hx711 import HX711
 
-DOUT = 5
-PD_SCK = 6
-
-def read_HX711():
-    count = 0
-    GPIO.output(PD_SCK, False)
-    
-    # Wait for the HX711 to become ready
-    while GPIO.input(DOUT) == 1:
-        pass
-    
-    # Read out 24-bit data from HX711
-    for i in range(24):
-        GPIO.output(PD_SCK, True)
-        count = count << 1
-        GPIO.output(PD_SCK, False)
-        if GPIO.input(DOUT) == 0:
-            count += 1
-            
-    # Set the channel and gain factor for next reading
-    GPIO.output(PD_SCK, True)
-    count ^= 0x800000  # XOR to flip the 25th bit
-    GPIO.output(PD_SCK, False)
-    
-    return count
-
-
-def get_weight():
-    value = read_HX711()
-    
-    # TODO: Calibrate the following values as per your load cell and HX711
-    reference_unit = 204  # Set to the value obtained during calibration
-    offset = 411352.82843137255  # Set to the value obtained during calibration
-    
-    weight = (value - offset) / reference_unit
-    return weight
-
-
 def main():
-    # Set up GPIO pins
     GPIO.setmode(GPIO.BCM)
-    # GPIO.setup(PD_SCK, GPIO.OUT)
-    # GPIO.setup(DOUT, GPIO.IN)
     hx = HX711(dout_pin=5, pd_sck_pin=6)
     
     hx.zero()
