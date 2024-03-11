56e788b5fd05d12b2bba4b1e1d8572b094842fa3
Scott MacDonnell <scottsmacdonnell@gmail.com>
2024-03-11 13:58:12 -0400

    Test status function

--- server/main.py
+++ server/main.py
@@ -1,13 +1,18 @@
 import RPi.GPIO as GPIO
 from hx711 import HX711
 
+def status(escape, message):
+    return '[' + escape + message + '\x1b[0m]'
+    
+
 def main():
+    print(status('\x1b[0;32;40', '  OK  '))
     GPIO.setmode(GPIO.BCM)
     hx = HX711(dout_pin=5, pd_sck_pin=6)
     
     hx.zero()
     
-    input('Place known weight on scale and press enter')
+    input('[' + '\x1b[0m]' + 'Place known weight on scale and press enter')
     reading = hx.get_data_mean(readings=100)
     
     known_weight = float(input('Enter the known weight in grams and press enter: '))
