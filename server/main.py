b779eb498577d1abba875665724eba7bd7cca688
Scott MacDonnell <scottsmacdonnell@gmail.com>
2024-03-11 13:59:28 -0400

    Minor changes

--- server/main.py
+++ server/main.py
@@ -2,17 +2,16 @@ import RPi.GPIO as GPIO
 from hx711 import HX711
 
 def status(escape, message):
-    return '[' + escape + message + '\x1b[0m]'
+    return '[' + escape + message + '\x1b[0m] '
     
 
 def main():
-    print(status('\x1b[0;32;40', '  OK  '))
     GPIO.setmode(GPIO.BCM)
     hx = HX711(dout_pin=5, pd_sck_pin=6)
     
     hx.zero()
     
-    input('[' + '\x1b[0m]' + 'Place known weight on scale and press enter')
+    input(status('\x1b[0;32;40', '  OK  ') + 'Place known weight on scale and press enter')
     reading = hx.get_data_mean(readings=100)
     
     known_weight = float(input('Enter the known weight in grams and press enter: '))
