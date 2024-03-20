7f6b849dda5757e7bd4f3be974597e9c6f0cca7d
Scott MacDonnell <scottsmacdonnell@gmail.com>
2024-03-20 15:14:34 -0400

    Create Xcode project

---
+++ app/Nutrisync/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Nutrisync
+//
+//  Created by Scott MacDonnell on 2024-03-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ app/Nutrisync/NutrisyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutrisyncApp.swift
+//  Nutrisync
+//
+//  Created by Scott MacDonnell on 2024-03-20.
+//
+
+import SwiftUI
+
+@main
+struct NutrisyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
